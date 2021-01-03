017df3f8c8d25dda3a0fecd914c0ecc844111fd6
Silthus <michael@reichenbach.in>
2021-01-03 22:38:46 +0100

    feat(movement-speed): add attribute modifier instead of modifying base value

--- modules/movement-speed/README.md
+++ modules/movement-speed/README.md
@@ -4,4 +4,6 @@ Erhöht den Basis Movement Speed des Spielers. [Hier ist eine Liste](https://min
 
 | Option | Default | Beschreibung |
 | ------ | ------- | ----------- |
-| `modifier` | `0.1` | Der Movement Speed Modifier des Spielers. `0.1` ist die normale Bewerbungsgeschwindigkeit. `0.15` ist Sprinten. |
+| `modifier` | `0.1` | Der Attribute Modifier der auf den Movement Speed des Spielers angewendet wird. Wichtig dabei ist das `operation` setting für die Anwendung des Werts. |
+| `operation` | `MULTIPLY_SCALAR_1` | Die [Operation](https://hub.spigotmc.org/javadocs/spigot/org/bukkit/attribute/AttributeModifier.Operation.html) mit der der `modifier` angewandt wird. `ADD_NUMBER`, `ADD_SCALAR` oder `MULTIPLY_SCALAR_1`. |
+| `states` | `ALL` | Die States des Spielers in denen der Speed Bonus angewendet werden soll. `IN_WATER`, `FLYING`, `WALKING`, `SPRINTING`, `GLIDING`, `SNEAKING`, `ALL` |
--- modules/movement-speed/src/main/java/de/raidcraft/skillsandeffects/skills/MovementSpeed.java
+++ modules/movement-speed/src/main/java/de/raidcraft/skillsandeffects/skills/MovementSpeed.java
@@ -1,16 +1,29 @@
 package de.raidcraft.skillsandeffects.skills;
 
+import com.google.common.base.Strings;
 import de.raidcraft.skills.AbstractSkill;
 import de.raidcraft.skills.SkillContext;
 import de.raidcraft.skills.SkillFactory;
 import de.raidcraft.skills.SkillInfo;
-import de.raidcraft.skills.configmapper.ConfigOption;
 import lombok.NonNull;
+import lombok.extern.java.Log;
+import org.bukkit.Location;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Log(topic = "RCSkills:movement-speed")
 @SkillInfo("movement-speed")
-public class MovementSpeed extends AbstractSkill {
+public class MovementSpeed extends AbstractSkill implements Listener {
 
     public static class Factory implements SkillFactory<MovementSpeed> {
 
@@ -26,32 +39,143 @@ public class MovementSpeed extends AbstractSkill {
 
     }
 
-    @ConfigOption
-    private double modifier = 0.1;
-    private double previousModifier = 0.1;
+    private AttributeModifier attributeModifier;
+    private Location lastLocation;
+    private State state;
+    private State oldState;
+
+    private final List<State> states = new ArrayList<>();
 
     protected MovementSpeed(SkillContext context) {
         super(context);
     }
 
     @Override
+    public void load(ConfigurationSection config) {
+
+        states.clear();
+        List<String> states = config.getStringList("states");
+        if (states.isEmpty() || states.contains("ALL")) {
+            this.states.addAll(Arrays.asList(State.values()));
+        } else {
+            for (String name : states) {
+                State state = State.fromString(name);
+                if (state != null) {
+                    this.states.add(state);
+                } else {
+                    log.warning("invalid movement state \"" + name + "\" in skill config " + context().configuredSkill().alias());
+                }
+            }
+        }
+
+        attributeModifier = new AttributeModifier(
+                context().configuredSkill().id(),
+                context().configuredSkill().alias(),
+                config.getDouble("modifier", 0.1),
+                AttributeModifier.Operation.valueOf(config.getString("operation", AttributeModifier.Operation.MULTIPLY_SCALAR_1.name()))
+        );
+    }
+
+    @Override
     public void apply() {
 
         context().player().ifPresent(player -> {
-            AttributeInstance attribute = player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED);
-            if (attribute == null) return;
-            previousModifier = attribute.getBaseValue();
-            attribute.setBaseValue(modifier);
+            updateState(player);
+            addSpeed(player);
         });
     }
 
     @Override
     public void remove() {
 
-        context().player().ifPresent(player -> {
-            AttributeInstance attribute = player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED);
-            if (attribute == null) return;
-            attribute.setBaseValue(previousModifier);
-        });
+        context().player().ifPresent(this::removeSpeed);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onMove(PlayerMoveEvent event) {
+
+        if (!hasMoved(event.getTo())) {
+            return;
+        }
+        lastLocation = event.getTo();
+        updateState(event.getPlayer());
+
+        if (stateChanged()) {
+            updateMovementSpeed(event.getPlayer());
+        }
+    }
+
+    private void updateMovementSpeed(Player player) {
+
+        if (states.contains(state)) {
+            addSpeed(player);
+        } else {
+            removeSpeed(player);
+        }
+    }
+
+    private void addSpeed(Player player) {
+
+        AttributeInstance attribute = player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED);
+        if (attribute == null) return;
+        attribute.addModifier(attributeModifier);
+    }
+
+    private void removeSpeed(Player player) {
+
+        AttributeInstance attribute = player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED);
+        if (attribute == null) return;
+        attribute.removeModifier(attributeModifier);
+    }
+
+    private boolean stateChanged() {
+
+        return oldState != state;
+    }
+
+    private void updateState(Player player) {
+
+        oldState = state;
+        if (player.isFlying()) {
+            state = State.FLYING;
+        } else if (player.isInWater()) {
+            state = State.IN_WATER;
+        } else if (player.isSprinting()) {
+            state = State.SPRINTING;
+        } else if (player.isGliding()) {
+            state = State.GLIDING;
+        } else if (player.isSneaking()) {
+            state = State.SNEAKING;
+        } else {
+            state = State.WALKING;
+        }
+    }
+
+    private boolean hasMoved(Location location) {
+
+        return lastLocation == null ||
+                location.getBlockX() != lastLocation.getBlockX()
+                || location.getBlockY() != lastLocation.getBlockY()
+                || location.getBlockZ() != lastLocation.getBlockZ();
+    }
+
+    enum State {
+        IN_WATER,
+        FLYING,
+        WALKING,
+        SPRINTING,
+        GLIDING,
+        SNEAKING;
+
+        public static State fromString(String state) {
+
+            if (Strings.isNullOrEmpty(state)) return null;
+            for (State value : values()) {
+                if (value.name().equalsIgnoreCase(state)) {
+                    return value;
+                }
+            }
+            return null;
+        }
     }
 }
